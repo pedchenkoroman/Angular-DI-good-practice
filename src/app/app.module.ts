b7eef4721a42b20a661ea39a4b0362584e99f1ad
pedchenkoroman <roman.pedchenko.88@gmail.com>
2022-07-10 15:25:26 +0200

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WidgetModule } from './widget/widget.module';
